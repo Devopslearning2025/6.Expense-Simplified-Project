b3b978b1449fa437e53ebfc3cb1df11e78d894b8
devops <dev0pslearning2025@gmail.com>
2024-04-23 21:25:45 +0530

    set

---
+++ set/common-code.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+handle_error(){
+    echo "Error occured at line no: $1 : $2"
+}
+
+trap'handle_error "$(LINENO)" "$BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+SCRIPT_NAME=$(echo $0|cut -d '.' -f1)
+DATE=$(date +%F-%H-%M-%S)
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+#echo "starting $SCRIPT_NAME shell script  at $DATE"
+
+check_user(){
+if [ $USERID -ne 0 ]
+then
+    echo "please run with root user"
+    exit 1
+else
+    echo "you are with root user"
+fi
+}
---
+++ set/mysql.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source ./common-code.sh
+check_user
+
+echo "Enter the DB Password"
+read -s mysql_password
+
+dnf install mysql-server -y &>>$LOGFILE
+#VALIDATE $? "Installing MySQL"
+
+systemctl enable mysqld &>>$LOGFILE
+#VALIDATE $? "enabling MYSQL"
+
+systemctl start mysqld &>>$LOGFILE
+#VALIDATE $? "starting MySQL"
+
+#mysql -h 172.31.24.255 -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
+mysql -h db.devopslearning2025.online -uroot -p${mysql_password} -e 'show databases;' &>>$LOGFILE
+
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_password} &>>$LOGFILE
+ #   VALIDATE $? "setting root password of MySQL"
+else
+    echo -e "MySQL passowrd already set ....$Y SKIPPING  $N"
+fi  

8955eb92dcc1691026da8df53ae333fa9370af46
devops <dev0pslearning2025@gmail.com>
2024-04-23 21:18:07 +0530

    set

--- set.sh
+++ set.sh
@@ -2,7 +2,7 @@
 set -e
 
 falilure(){
-    echo "Line number: $1 , Command:$2"
+    echo "Line number: $1 : $2"
 }
 trap '"${LINENO}" "$BASH_COMMAND"' ERR
 
@@ -18,7 +18,7 @@ systemctl enable mysqld &>>$LOGFILE
 systemctl start mysqld &>>$LOGFILE
 
 #mysql -h 172.31.24.255 -uroot -pExpenseApp@1 -e 'show databases;' &>>$LOGFILE
-mysql -h db.devopslearning2025.onlinee -uroot -p${mysql_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.devopslearning2025.online -uroot -p${mysql_password} -e 'show databases;' &>>$LOGFILE
 
 if [ $? -ne 0 ]
 then

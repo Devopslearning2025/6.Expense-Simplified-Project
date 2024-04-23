9e13b595a269a812a232e43afa3804846e9af390
devops <dev0pslearning2025@gmail.com>
2024-04-23 21:43:27 +0530

    set

---
+++ set/frontend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ./common-code.sh
+check_user
+
+dnf install nginx -y  &>>$LOGFILE
+systemctl enable nginx  &>>$LOGFILE
+systemctl start nginx &>>$LOGFILE
+
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip  &>>$LOGFILE
+cd /usr/share/nginx/html
+rm -rf /usr/share/nginx/html/* 
+unzip /tmp/frontend.zip &>>$LOGFILE
+cp /home/ec2-user/simplified-project/set/expense.conf /etc/nginx/default.d/expense.conf
+
+systemctl restart nginx&>>$LOGFILE
